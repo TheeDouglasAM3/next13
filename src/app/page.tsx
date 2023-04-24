381a615fb9f1813029ed92f29e616776753c437e
Douglas Alves Marcelino <douglas.alves@havan.com.br>
2023-04-24 17:32:25 -0300

    feat: adiciona exemplos de fetches e rotas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <h1>Hello World</h1>
+import Link from 'next/link'
+
+export default async function Home() {
+  // revalidate
+  const response = await fetch('https://api.github.com/users/theedouglasam3', {
+    next: {
+      revalidate: 30,
+    },
+  })
+
+  // SSR
+  // const response = await fetch('https://api.github.com/users/theedouglasam3', {
+  //   cache: 'no-store'
+  // })
+
+  const user = await response.json()
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <Link href="/dashboard">Dashboard</Link>
+      <Link href="/signin">Signin</Link>
+    </div>
+  )
 }
